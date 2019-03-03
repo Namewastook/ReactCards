5b40d4f807493e48787e9aacf33296e30c78095d
TJ <philosophy.cheer@gmail.com>
2019-03-03 14:18:14 -0500

    Updates

---
+++ precache-manifest.3f0afd0b5bb85ab8d143eaf8d3dc262f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "886ffe08412b2c804285",
+    "url": "/ReactCards/static/js/runtime~main.886ffe08.js"
+  },
+  {
+    "revision": "ba1a205423a139730986",
+    "url": "/ReactCards/static/js/main.ba1a2054.chunk.js"
+  },
+  {
+    "revision": "54094b48b410ee26b80e",
+    "url": "/ReactCards/static/js/2.54094b48.chunk.js"
+  },
+  {
+    "revision": "ba1a205423a139730986",
+    "url": "/ReactCards/static/css/main.e2c8af66.chunk.css"
+  },
+  {
+    "revision": "f73df8820bbe869fef5463e1b1a0758e",
+    "url": "/ReactCards/index.html"
+  }
+];
